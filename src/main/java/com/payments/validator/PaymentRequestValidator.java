1ac2054f0605d74833bb50c1ab1d1ae952c7e7a4
medhini <medhini.sp@gmail.com>
2022-05-04 17:21:28 +0530

    added stereotypes

--- src/main/java/com/payments/validator/PaymentRequestValidator.java
+++ src/main/java/com/payments/validator/PaymentRequestValidator.java
@@ -6,6 +6,7 @@
  */
 
 package com.payments.validator;
+import org.springframework.stereotype.Component;
 
 import com.payments.model.PaymentRequest;
 
@@ -13,6 +14,7 @@ import com.payments.model.PaymentRequest;
  * @author Tumukunta
  *
  */
+@Component
 public class PaymentRequestValidator {
 
 	public void validateRequest(PaymentRequest paymentRequest) {
